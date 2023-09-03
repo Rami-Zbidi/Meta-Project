dceb96bbdf3a1a585a6fa64a9f8282b7bb8a7526
Rami-Zbidi <rami.zbidi@horizon-tech.tn>
2023-09-03 04:21:44 +0100

    Edit App.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import "../App.css";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 import Logo from "../images/littleLemonLogo.png";
 
 function Header()
 {
     return(
         <header className="header">
-            <img className="logo" src={Logo} alt="Little Lemon Logo" />
+            <Link to="/"><img className="logo" src={Logo} alt="Little Lemon Logo" /></Link>
             <Nav />
         </header>
     );
